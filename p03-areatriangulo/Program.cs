a8ff168dc40e0ea3de442a8947bd50c29e74dd90
joanb <joanbres.98@gmail.com>
2021-08-25 20:29:24 -0500

    va

---
+++ p03-areatriangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿//Calcular el are de un triangulo
+
+using System;
+
+namespace p03_areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float b,h,a;
+            b=h=a=0;
+            Console.WriteLine("calculando el area de un triangulo\n");
+            Console.Write("Dame la base ");
+            b=float.Parse(Console.ReadLine());
+            Console.Write("Dame la altura ");
+            h=float.Parse(Console.ReadLine());
+            a=(b*h)/2;
+            Console.WriteLine($"El area del triangulo es {a}");
+        }
+    }
+}
