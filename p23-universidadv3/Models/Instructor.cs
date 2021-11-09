503baad549d69be01f9dae9e4f2e487b6aba439d
joanb <joanbres.98@gmail.com>
2021-11-09 15:55:13 -0600

    hey

---
+++ p23-universidadv3/Models/Instructor.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace p21_universidadv1.Modelo{
+    public class Instructor : Persona{
+        [DataType(DataType.Date), DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+        public DateTime FechaContratacion {get; set;}
+        //public int OficinaId{get;set;}
+        //public int DepartamentoId{get;set;}
+
+        public ICollection<AsignacionCurso> AsignacionCursos{get;set;}
+        //public Departamento Departamento{get;set;}
+        public OficinaAsignada OficinaAsignada{get;set;}
+   }
+    
+}
