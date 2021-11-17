b6dd768d12ce8a56085fcb2a57a08eb587f06222
joanb <joanbres.98@gmail.com>
2021-11-16 20:29:18 -0600

    hooo

--- p23-universidadv3/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ p23-universidadv3/obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,77 +13,77 @@ namespace p21_universidadv1.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 1 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 2 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 3 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 4 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 5 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 6 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 7 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 8 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 9 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using p21_universidadv1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 10 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using p21_universidadv1.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\Pages\FetchData.razor"
+#line 3 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\Pages\FetchData.razor"
 using p21_universidadv1.Data;
 
 #line default
@@ -98,7 +98,7 @@ using p21_universidadv1.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\Pages\FetchData.razor"
+#line 39 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
--- p23-universidadv3/obj/Debug/net5.0/RazorDeclaration/Pages/Instructor/AgregarInstructores.razor.g.cs
+++ p23-universidadv3/obj/Debug/net5.0/RazorDeclaration/Pages/Instructor/AgregarInstructores.razor.g.cs
@@ -13,84 +13,84 @@ namespace p21_universidadv1.Pages.Instructor
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 1 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 2 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 3 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 4 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 5 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 6 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 7 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 8 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 9 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using p21_universidadv1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
+#line 10 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\_Imports.razor"
 using p21_universidadv1.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\Pages\Instructor\AgregarInstructores.razor"
+#line 4 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\Pages\Instructor\AgregarInstructores.razor"
 using p21_universidadv1.Modelo;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\Pages\Instructor\AgregarInstructores.razor"
+#line 5 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\Pages\Instructor\AgregarInstructores.razor"
 using p21_universidadv1.Servicio;
 
 #line default
@@ -105,7 +105,7 @@ using p21_universidadv1.Servicio;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 41 "C:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\Pages\Instructor\AgregarInstructores.razor"
+#line 41 "c:\Users\joanb\Documents\ING\9no\desarrollo de aplicacaciones web\programas\p23-universidadv3\Pages\Instructor\AgregarInstructores.razor"
        
     Instructor obj = new Instructor();
     protected void CrearInstructor() {
